c760a6e563452f5f6811a4596a68a6332ca6f992
Harsh Jain <66999104+Harshjain007@users.noreply.github.com>
2021-08-12 02:05:32 +0530

    Create HourGlass.java

---
+++ HourGlass.java
@@ -0,0 +1,41 @@
+public class HourGlass {
+    public static void main(String[] args) {
+        int n=6;
+        
+
+        for(int i=n;i>0;i--){
+            for(int j=0;j <(n-i) ;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j > 0;j--){
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=n-i;j>1;j--){
+                System.out.print(" ");
+                }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+                }
+            System.out.println( );
+        }
+
+/*
+
+* * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+* * * * * * 
+
+*/
