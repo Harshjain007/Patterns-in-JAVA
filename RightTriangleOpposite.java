1acdd6419f6b86002d56b588a2b621d5c0beec48
Harsh Jain <66999104+Harshjain007@users.noreply.github.com>
2021-08-12 02:09:12 +0530

    Create RightTriangleOpposite.java

---
+++ RightTriangleOpposite.java
@@ -0,0 +1,29 @@
+public class RightTriangleOpposite {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=n-i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+            ******
+             *****
+              ****
+               ***
+                **
+                 *
+                 
+                 */
+      
+      
+      
+    }
+}
+      
